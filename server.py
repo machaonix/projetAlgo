b62858fabf14e64799a98456383b01df2ce85448
machaonix <machaonix@protonmail.com>
2020-11-28 15:30:02 +0100

    server passer en multithreadé

--- server.py
+++ server.py
@@ -1,4 +1,17 @@
 import socket
+import threading
+
+class Thread_for_client(threading.Thread):
+	def __init__(self, connection):
+		threading.Thread.__init__(self)
+		self.connection = connection
+
+	def run(self):
+		while True:
+			user_text = connection.recv(8192)
+			connection.send(user_text)
+			print(user_text.decode("utf8"))
+
 
 host, port = ('', 7777)
 
@@ -8,13 +21,11 @@ socketServer.bind((host, port))
 print("Server démarré...")
 
 while True:
-    print("En Écoute")
-    socketServer.listen(5)
-    connection, addresse = socketServer.accept()
-    while True:
-        user_text = connection.recv(8192)
-        connection.send(user_text)
-        print(user_text.decode("utf8"))
+	print("En Écoute")
+	socketServer.listen(5)
+	connection, addresse = socketServer.accept()
+	leThread = Thread_for_client(connection)
+	leThread.start()
 
 connection.close()
 socketServer.close()
