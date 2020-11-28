7534f98fe6ec583dbe6c6df127b0c70384cb0b38
machaonix <machaonix@protonmail.com>
2020-11-28 16:56:47 +0100

    Ajout d'un base de donnée

--- client.py
+++ client.py
@@ -8,6 +8,8 @@ try:
 	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	clientSocket.connect((host, port))
 	print("Client connécté")
+	pseudo = input("Entrez un psudonyme\n>>>")
+	clientSocket.send(pseudo.encode("utf8"))
 	while launched:
 		user_text = input(">>>")
 		clientSocket.send(user_text.encode("utf8"))
--- server.py
+++ server.py
@@ -1,18 +1,42 @@
 import socket
 import threading
+import sqlite3
 
 class Thread_for_client(threading.Thread):
-	def __init__(self, connection):
+	def __init__(self, connection, curseur):
 		threading.Thread.__init__(self)
+		self.curseur = curseur
 		self.connection = connection
 
 	def run(self):
+		user_pseudo = self.connection.recv(1024)
 		while True:
-			user_text = connection.recv(8192)
-			connection.send(user_text)
+			sql = f"""SELECT * FROM tMessages
+					WHERE destinataire = '{user_pseudo}'
+					ORDER BY dateDEnvoi
+			"""
+			userMessageconnection.recv(8192)
+			resultat = curseur.excecute(sql)
+			dernierMessage = resultat.fetchone()
+			if(isinstance(dernierMessage, str))
+				self.connection.send(dernierMessage.encode("utf8"))
 			print(user_text.decode("utf8"))
 
+#----------------------------------------------------------------------------------------------
+#proKiloGramme principal
 
+
+
+connexionALaBaseDeDonnees = sqlite3.connect('NOTRE_BASE_DE_DONNEES.db')
+curseur = connexionALaBaseDeDonnees.cursor()
+sql = """CREATE TABLE IF NOT EXISTS tMessages (
+    expediteur VARCHAR(25) NOT NULL,
+    destinataire VARCHAR(25),
+    dateDEnvoi DATETIME DEFAULT NOW() NOT NULL,
+    contenu VARCHAR(255) NOT NULL,
+    idMessage INTEGER PRIMARY KEY  AUTO_INCREMENT
+);"""
+curseur.excecute(sql)
 host, port = ('', 7777)
 
 socketServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,10 +46,10 @@ print("Server démarré...")
 
 while True:
 	print("En Écoute")
-	socketServer.listen(5)
-	connection, addresse = socketServer.accept()
-	leThread = Thread_for_client(connection)
-	leThread.start()
+	socketServer.listen(5) # <---- s'arrete et écoute 
+	connection, addresse = socketServer.accept()# des qu'il trouve qqn il l'accepte
+	leThread = Thread_for_client(connection, curseur)# declaration du thread
+	leThread.start()# lancement du thread (ce qui il y a dans run())
 
 connection.close()
 socketServer.close()
