cfe71badb92f2778554e8a0b732210a9e53ec20c
machaonix <machaonix@protonmail.com>
2020-12-28 16:42:07 +0100

    Correction de bug dans Date.c

--- source/Date.c
+++ source/Date.c
@@ -16,14 +16,14 @@ void afficherDate(Date date, FILE* flux)
 int dateCmp(Date date1, Date date2)
 {
 	int nbjour;
-	short tabMois[12] = {0, 31, 59, 89, 120, 150, 181, 212, 242, 273, 303, 334};//Nombres de jours dans un mois cummulé
-	
-	nbjour = abs((date1.annee-date2.annee)*tabMois[11]);
+	short tabMois[12] = {0, 31, 59, 89, 120, 151, 181, 212, 243, 273, 304, 334};//Nombres de jours dans un mois cummulé
+
+	nbjour = abs((date1.annee-date2.annee)*tabMois[10]);
 	nbjour += abs(tabMois[date1.mois-1] - tabMois[date2.mois-1]);
 	nbjour += abs(date1.jour - date2.jour);
 	for(int i=plusPetiteDate(&date1, &date2)->annee; i<=plusGrandeDate(&date1, &date2)->annee; ++i)
 	{
-		if(i%4 == 0 && plusPetiteDate(&date1, &date2)->mois > 2)
+		if(i%4 == 0 && plusPetiteDate(&date1, &date2)->mois >= 2)
 		{
 			++nbjour;
 			if(i%100 == 0)
@@ -45,7 +45,7 @@ Date *plusPetiteDate(Date *date1, Date *date2)
 {
 	if(date1->annee < date2->annee)
 		return date1;
-	if(date1->annee < date2->annee)
+	if(date1->annee > date2->annee)
 		return date2;
 
 	if(date1->mois < date2->mois)
