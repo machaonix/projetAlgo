b9ffaaa611865a134b4b7dea52468624ec10659a
machaonix <machaonix@protonmail.com>
2021-01-02 11:56:42 +0100

    Documentation de Date.h

--- source/Date.h
+++ source/Date.h
@@ -11,16 +11,58 @@ typedef struct {
   int annee;
 } Date;
 
+/*
+lireDate():
+    Retour		: Une date lue dans un flux
+    Argument	:
+        flux -> Un pointeur vers un flux en mode lecture ou le cursseur se trouve juste avant la date
+	Finalitée	:
+		Lire une date dans un flux
+*/
 Date lireDate(FILE* flux);
+
+/*
+afficherDate():
+	Retour 		: void
+	Arguments	:
+		date -> Une date à afficher
+		flux -> Un pointeur de flux en mode écriture où ecrire date
+	Finalité:
+		Ecrire une date sur un flux
+*/
 void afficherDate(Date date, FILE* flux);
 
-//          dateCmp
-//retourne ecart en jour    negative si date1<date2
-//                          positive si date1>date2
-//                          nulle    si date1==date2     
+/*
+dateCmp():
+	Retour 		: ecart en jour (date1 - date2)
+	Arguments	:
+		date1 -> Une Date destinée à être comparée avec date2
+		date2 -> Une Date destinée à être comparée avec date1
+	Finalité:
+		Comparer 2 dates
+*/
 int dateCmp(Date date1, Date date2);
 
+/*
+plusPetiteDate():
+	Retour 		: Pointeur de la Date la plus petite
+	Arguments	:
+		date1 -> Un pointeur de Date destinée à être comparée avec date2
+		date2 -> Un pointeur de Date destinée à être comparée avec date1
+	Finalité :
+			Déterminer la plus petite date
+*/
 Date *plusPetiteDate(Date *date1, Date *date2);
+
+/*
+plusGrandeDate():
+	Retour 		: Pointeur de la Date la plus grande
+	Arguments	:
+		date1 -> Un pointeur de Date destinée à être comparée avec date2
+		date2 -> Un pointeur de Date destinée à être comparée avec date1
+	Finalité :
+			Déterminer la plus grande date
+*/
 Date *plusGrandeDate(Date *date1, Date *date2);
 
 #endif //__DATE_H__
