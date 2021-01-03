967ad10a2c0f779c57b51e9cf1be1a41acd1b9d0
Mathis Ribémont <mathisribemont@gmail.com>
2021-01-03 11:23:21 +0100

    + Commentaire dans EmpruntReservation.c

--- source/EmpruntReservation.c
+++ source/EmpruntReservation.c
@@ -1,5 +1,13 @@
 #include "EmpruntReservation.h"
 
+/*
+    lireEmpruntReservation
+Description :
+  Lit les données d'un fichier pour les mettre dans un emprunt qui sera retourné.
+
+Argument :
+  FILE* flux -> flux du fichier à lire.
+*/
 EmpruntReservation lireEmpruntReservation(FILE* flux)
 {
   EmpruntReservation er;
@@ -8,6 +16,16 @@ EmpruntReservation lireEmpruntReservation(FILE* flux)
   return er;
 }
 
+
+/*
+    afficherEmpruntReservation
+Description :
+  Affiche les données d'un emprunt dans un flux.
+
+Argument :
+  EmpruntReservation *er -> pointeur sur l'emprunt à afficher.
+  FILE* flux -> flux de destination des données (stdout ou un fichier)
+*/
 void afficherEmpruntReservation(EmpruntReservation *er, FILE* flux)
 {
   fprintf(flux,"%u\t%u\t%u\t\t",er->id,er->idJeu,er->idEmprunter);
@@ -15,7 +33,15 @@ void afficherEmpruntReservation(EmpruntReservation *er, FILE* flux)
   fprintf(flux, "\n");
 }
 
-//interactif
+
+/*
+    nouvEmpruntReservation
+Description :
+  Créer un emprunt ou une reservation, en demandant les informations de l'emprunt à l'utilisateur.
+  
+Arguments :
+  unsigned int id -> identifiant de l'emprunt ou de la reservation
+*/
 EmpruntReservation nouvEmpruntReservation(unsigned int id)
 {
   EmpruntReservation er;
